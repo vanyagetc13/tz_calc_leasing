c6a5253ee08f017142f381c6a1970a79ab104134
vanyagetc13 <bugaev-ivan2k16@yandex.ru>
2022-10-03 23:06:36 +0300

    version control(to see if changes deployed to gh-pages) in the top-left corner

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ function App() {
 
     return (
         <div className="App">
+            <div className="version">v 2.0.1a</div>
             <MyTitle />
             <div className="inputs">
                 <DragInput
